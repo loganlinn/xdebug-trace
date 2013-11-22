da12f44eb0b4f5a2ab100067fa5dd79047b0f34c
loganlinn <logan.linn@gmail.com>
2013-11-22 11:43:50 -0500

    Cache trace renderings based on file mod time

--- src/xdebug_trace/service.clj
+++ src/xdebug_trace/service.clj
@@ -8,14 +8,17 @@
             [xdebug-trace.util :refer [distinct-on]]
             [clojure.java.io :as io]
             [clojure.string :as str]
+            [clojure.pprint :refer [pprint]]
             [ring.middleware.params :refer [wrap-params]]
             [ring.middleware.resource :refer [wrap-resource]]
-            [ring.util.response :as resp]
+            [ring.util.response :as res]
+            [ring.util.time :as time]
             [compojure.core :refer [routes GET POST ANY]]
             [compojure.route :as route]
             [compojure.handler]
             [environ.core :refer [env]])
-  (:import [java.io File FileFilter]))
+  (:import [java.io File FileFilter]
+           [java.util Date]))
 
 (def trace-ext ".xt")
 
@@ -53,19 +56,35 @@
            (reader/read-trace)
            (view.trace/render-trace)))))
 
+(defn not-modified-since?
+  [{headers :headers :as req} last-modified]
+  (if-let [ims (some-> (headers "if-modified-since") time/parse-date)]
+    (not (.before ims last-modified))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Public
 
+(defn create-handler [trace-dirs]
+  (routes
+    (GET "/" [] (view.index/index))
+    (GET "/trace/:trace-name" {{:keys [trace-name limit offset]} :params :as req}
+         (if-let [^File trace-file (find-trace trace-dirs trace-name)]
+           (let [lmodified (Date. (.lastModified trace-file))]
+             (if (not-modified-since? req lmodified)
+               (-> (res/response "") (res/status 304))
+               (-> (res/response (get-trace trace-file limit offset))
+                   (res/header "ETag" "123123123")
+                   (res/header "Last-Modified"
+                               (time/format-date lmodified))
+                   (res/header "Cache-Control"
+                               "no-transform,public,max-age=60,s-maxage=60"))))))
+    (GET "/trace" []
+         (let [trace-files (find-traces trace-dirs)]
+           (view.trace/render-traces trace-files)))))
+
 (defn create-app [trace-path]
   (let [trace-dirs (map io/file (str/split trace-path #":"))]
-    (-> (routes
-          (GET "/" [] (view.index/index))
-          (GET "/trace/:trace-name" [trace-name limit offset]
-               (if-let [trace-file (find-trace trace-dirs trace-name)]
-                 (get-trace trace-file limit offset)))
-          (GET "/trace" []
-               (let [trace-files (find-traces trace-dirs)]
-                 (view.trace/render-traces trace-files))))
+    (-> (create-handler trace-dirs)
         (view.layout/wrap-with-request)
         (wrap-resource "public")
         (compojure.handler/site))))
