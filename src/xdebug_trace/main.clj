d72905e4dfd96410a412b6fb757fcd3a70e355b0
loganlinn <logan.linn@gmail.com>
2013-11-30 11:48:46 -0500

    Generate main ns

--- src/xdebug_trace/main.clj
+++ src/xdebug_trace/main.clj
@@ -1,8 +1,8 @@
 (ns xdebug-trace.main
-  (:gen-class)
+  (:gen-class :main :true)
   (:require [xdebug-trace.system :as system]))
 
 (defn -main
   [& args]
   (-> (system/system)
-      (system/start)))
+      (system/start))
