9e6857fb9268ef44ef28d8c4a59cdeb40a7bef1c
loganlinn <logan.linn@gmail.com>
2013-11-30 21:55:41 -0500

    Install lein-daemon plugin
    Exactly what I needed. Thanks, @arohner

--- project.clj
+++ project.clj
@@ -11,10 +11,13 @@
                  [compojure "1.1.6"]
                  [hiccup "1.0.4"]
                  [clj-time "0.6.0"]]
+  :plugins [[lein-daemon "0.5.4"]]
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.4"]
                                   [org.clojure/java.classpath "0.2.0"]]}
              :uberjar {:main xdebug-trace.main
                        :aot [xdebug-trace.main]}
              :jdk6 {:dependencies [[org.codehaus.jsr166-mirror/jsr166y "1.7.0"]]}}
-  :resource-paths ["resources" "upload"])
+  :resource-paths ["resources" "upload"]
+  :daemon {:server {:ns xdebug-trace.main
+                    :pidfile "xdebug-trace-jetty.pid"}})
